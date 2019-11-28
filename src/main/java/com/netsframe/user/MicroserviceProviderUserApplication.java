8439b4905058d1323506e7093b7ba2c06ef858a4
chenhao <chenhao@authine.com>
2019-11-28 14:47:53 +0800

    11

--- src/main/java/com/netsframe/user/MicroserviceProviderUserApplication.java
+++ src/main/java/com/netsframe/user/MicroserviceProviderUserApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.netsframe.user", "org.mircoservice.org"})
-//@EnableEurekaClient // 启动服务注册
+//@EnableEurekaClient // 启动服务注册,只能使用eureka作为注册中心
 @EnableFeignClients // 启动feign
-@EnableDiscoveryClient
+@EnableDiscoveryClient //启动服务注册，可以使用eureka、zookeeper、consul作为注册中心
 //@EnableCircuitBreaker // 开启Hystrix熔断
 //@EnableHystrixDashboard
 public class MicroserviceProviderUserApplication {
