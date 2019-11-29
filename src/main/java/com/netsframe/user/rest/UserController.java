a4def78511e2cfa44ec65a113a27f698811899fb
chenhao <chenhao@authine.com>
2019-11-29 09:47:05 +0800

    11

--- src/main/java/com/netsframe/user/rest/UserController.java
+++ src/main/java/com/netsframe/user/rest/UserController.java
@@ -10,14 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private OrgService orgService;
-//	@Autowired
-//	OrgServiceClient orgServiceClient;
 
     @GetMapping("/user/query/{id}")
     public String query(@PathVariable("id") String id) {
         System.out.println("id=" + id);
         String org = orgService.queryOrg(id);
-//		String org = orgServiceClient.queryOrg(id);
         return "昊哥哥所属组织：" + org;
     }
 }
